9bb78f8f245c9dc8a77a263d4e0b3a9d2e1443a1
Joshua Bradley <bradleyjd19@gmail.com>
2019-09-13 12:13:13 -0500

    header and footer responsiveness

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -5,14 +5,13 @@ function Footer() {
 
   return (
     <footer className="page-footer font-small">
-      <div className="container d-flex justify-content-center" id="container-footer">
+      <div className="container" id="container-footer">
         <div className="row" id="row-footer">
-          <div className="column text-center my-auto" id="footer-links">
-            <div id="address">Nicholson Capital Management</div>
-            <div id="address">4600 Madison Avenue, Suite 150</div>
-            <div id="address">Kansas City, MO  64112</div>
-            <div id="address">Phone: 816-931-5757</div>
-            <div id="address">Fax: 816-531-0418</div>
+          <div className="column my-auto" id="footer-links">
+            <div id="address"><i className="fas fa-map-marker-alt ml-1 mr-2"></i>4600 Madison Avenue, Suite 150</div>
+            <div id="address" className="ml-4">Kansas City, MO {'\u00A0'} 64112</div>
+            <div id="address"><i className="fas fa-mobile-alt ml-1 mr-2"></i>816-931-5757</div>
+            <div id="address"><i className="fas fa-fax mr-2"></i>816-531-0418</div>
           </div>
         </div>
       </div>
